25ab8dd1631f5595d4bec4b49e208a50abf3c74f
Tejal <tejalpawar960094@gmail.com>
2025-04-08 11:54:18 +0530

    Added new exceptions

--- EmpJdbcProject/src/com/hexaware/exceptions/EmpNotFoundException.java
+++ EmpJdbcProject/src/com/hexaware/exceptions/EmpNotFoundException.java
@@ -2,4 +2,32 @@ package com.hexaware.exceptions;
 
 public class EmpNotFoundException extends Exception {
 
+	public EmpNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
 }
