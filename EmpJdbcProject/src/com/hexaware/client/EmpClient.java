78a7824edbbcf508634c3b9f95df859112ddf066
vaishnavip1311 <vaishnavip196@gmail.com>
2025-04-08 12:20:10 +0530

    added display method

--- EmpJdbcProject/src/com/hexaware/client/EmpClient.java
+++ EmpJdbcProject/src/com/hexaware/client/EmpClient.java
@@ -4,7 +4,13 @@ public class EmpClient {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
+		display();
 
+	}
+	
+	
+	public static void display() {
+		System.out.println("Display");
 	}
 
 }
