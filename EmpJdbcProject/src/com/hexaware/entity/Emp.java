567b1c5a33cfbc3565e58e469d30c8fac359209e
Tejal <tejalpawar960094@gmail.com>
2025-04-08 11:57:47 +0530

    Added entity package with emp class

---
+++ EmpJdbcProject/src/com/hexaware/entity/Emp.java
@@ -0,0 +1,26 @@
+package com.hexaware.entity;
+
+public class Emp {
+	
+	private int empId;
+	private String empName;
+	private double empSal;
+	public Emp(int empId, String empName, double empSal) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	
+	
+
+}
