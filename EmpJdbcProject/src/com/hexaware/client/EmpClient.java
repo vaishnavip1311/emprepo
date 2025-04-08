97c2bc6df0b6778c40ed0478af09d5f8a0ee0a38
Tejal <tejalpawar960094@gmail.com>
2025-04-08 12:34:07 +0530

    Resolved conflict of EmpClient.java

--- EmpJdbcProject/src/com/hexaware/client/EmpClient.java
+++ EmpJdbcProject/src/com/hexaware/client/EmpClient.java
@@ -4,13 +4,17 @@ public class EmpClient {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-		display();
 
-	}
-	
-	
+		display();
+		viewResults();
+
+}
 	public static void display() {
 		System.out.println("Display");
+	}
+	public static void viewResults()
+	{
+		System.out.println("View results");
 	}
 
 }
